c3ddf78495eff1e34bf8c16ab7b4f7a8f04ea86a
shibd <wudixiaolongbao@gmail.com>
2020-08-26 16:42:14 +0800

    feat: 调通helloworld

--- hello-world/domain/src/test/java/com/iquantex/samples/helloworld/domain/HelloAggregateTest.java
+++ hello-world/domain/src/test/java/com/iquantex/samples/helloworld/domain/HelloAggregateTest.java
@@ -1,8 +1,8 @@
 package com.iquantex.samples.helloworld.domain;
 
+import com.iquantex.phoenix.server.test.EntityAggregateFixture;
 import com.iquantex.samples.helloworld.coreapi.Hello;
 import com.iquantex.samples.helloworld.domain.entity.HelloAggregate;
-import com.iquantex.phoenix.server.test.util.EntityAggregateFixture;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -13,7 +13,7 @@ public class HelloAggregateTest {
 
 	@Test
 	public void testHello() {
-		EntityAggregateFixture fixture = new EntityAggregateFixture();
+		EntityAggregateFixture fixture = new EntityAggregateFixture(HelloAggregate.class.getPackage().getName());
 		Hello.HelloCmd helloCmd = Hello.HelloCmd.newBuilder().setHelloId("H001").build();
 		// 断言事件
 		fixture.when(helloCmd).expectRetSuccessCode().expectMessage(Hello.HelloEvent.class);
