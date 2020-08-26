068d547050357adf775a485e922e7401dd889d15
shibd <wudixiaolongbao@gmail.com>
2020-08-26 17:52:09 +0800

    feat: 解决包冲突

--- hello-world/application/src/main/java/com/iquantex/samples/helloworld/HelloWorldApplication.java
+++ hello-world/application/src/main/java/com/iquantex/samples/helloworld/HelloWorldApplication.java
@@ -3,10 +3,8 @@ package com.iquantex.samples.helloworld;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Slf4j
-@EnableSwagger2
 @SpringBootApplication
 public class HelloWorldApplication {
 
